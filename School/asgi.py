de37edc7dd818c989ef6dac8e2c5844de3eab03b
Christopher Fauconnier <64019350+Lamia-classy@users.noreply.github.com>
2021-03-24 20:14:48 +0100

    Revert "modif repository"
    This reverts commit f5e3d3d7c0d77e260960d1bd33628f1067748fc4.

--- School/asgi.py
+++ School/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Azakana project.
+ASGI config for School project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Azakana.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'School.settings')
 
 application = get_asgi_application()
