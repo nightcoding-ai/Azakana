45ffd4464d9f947657517280eadf4d8a9ef227c2
Christopher Fauconnier <64019350+Lamia-classy@users.noreply.github.com>
2021-03-26 21:09:57 +0100

    changement infrastructure projet
    modification des fichiers et build react

--- School/urls.py
+++ School/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Home.urls')),
-    path('', TemplateView.as_view(template_name='home.html')),
+    #path('', include('Home.urls')),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('api-auth/', include('rest_framework.urls')),
 ]
---
+++ gui/src/App.js
@@ -0,0 +1,12 @@
+
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+        <Header />
+    </div>
+  );
+}
+
+export default App;
