5a893513447b0b309ac10c1ff20e4a875278a796
Simon Kinet <64420845+Nepsylon@users.noreply.github.com>
2021-03-27 19:12:50 +0100

    Bon début de page

--- gui/src/components/HeroSection.js
+++ gui/src/components/HeroSection.js
@@ -7,25 +7,28 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
-      <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-        >
-          GET STARTED
-        </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('hey')}
-        >
-          WATCH TRAILER <i className='far fa-play-circle' />
-        </Button>
-      </div>
+      <div className="search">
+      <select className="servers">
+        <option value="NA">NA</option>
+        <option value="EUW">EUW</option>
+        <option value="EUNA">EUNA</option>
+        <option value="TU">TU</option>
+        <option value="JP">JP</option>
+        <option value="OC">OC</option>
+      </select>
+
+      <label for="summoner"></label>
+
+      <input
+        type="search"
+        id="site-search"
+        className="input"
+        aria-label="Search through site content"
+        placeholder='Ton pseudo'
+      />
+
+      <button className="Research">Rechercher</button>
+    </div>
     </div>
   );
 }
