419844590a0675ad975a41a6915ac6d7941bf968
Disdix10 <44231661+Disdix10@users.noreply.github.com>
2021-04-10 17:11:23 +0200

    création dossier api

--- School/urls.py
+++ School/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include,re_path
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('Home.urls')),
+    path('api/', include('api.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
+    path('auth/', obtain_auth_token),
     re_path(r'^statistiques', TemplateView.as_view(
         template_name='index.html')),
     re_path(r'^team', TemplateView.as_view(template_name='index.html')),
