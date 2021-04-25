048b1dc9ab8e9e811a208d478d3f73587c054ff9
Christopher Fauconnier <64019350+Lamia-classy@users.noreply.github.com>
2021-04-25 19:58:21 +0200

    requête améliorée
    nouveau div qui affiche le contenu spécifié dans la requête

--- frontend/src/components/Body_home/Middle.js
+++ frontend/src/components/Body_home/Middle.js
@@ -12,6 +12,7 @@ class Middle extends React.Component{
   handleClick(){
     let serveur = document.getElementById('servers').value;
     let nom = document.getElementById('summoner').value;
+    let section = document.getElementById('fill-in');
     const API_DEV = 'RGAPI-0d079bac-c38c-4ae7-a9ec-0e630a4369b5';
     axios.get(`https://`+serveur+`.api.riotgames.com/lol/summoner/v4/summoners/by-name/`+nom+`?api_key=`+API_DEV)
       .then(res => {
@@ -19,11 +20,11 @@ class Middle extends React.Component{
         data = res.data;
         url = res.config.url;
         console.log(data, url);
+        section.innerHTML = "Nom: "+String(data.name)+"<br>Niveau: "+String(data.summonerLevel);
       })
       .catch(error => {
         console.log(error.response)
       })
-    //alert(serveur + ' ' + nom);
   }
   
 
@@ -36,7 +37,7 @@ class Middle extends React.Component{
             <option value="euw1">EUW</option>
             <option value="na1">NA</option>
             <option value="euna1">EUNA</option>
-            <option value="tu1">TU</option>
+            <option value="tr1">TR</option>
             <option value="jp1">JP</option>
             <option value="oc1">OC</option>
             <option value="kr">KR</option>
@@ -52,6 +53,7 @@ class Middle extends React.Component{
            />
 
         <button onClick={this.handleClick} className="Research">Rechercher</button>
+        <div id='fill-in'>div à remplir avec la requête</div>
         </div>
     </div>
     );
