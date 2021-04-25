d482758b0908086faa6a924690ac7892a17a29ea
Simon Kinet <64420845+Nepsylon@users.noreply.github.com>
2021-04-26 00:04:28 +0200

    Profil fini mais pas responsive

--- frontend/src/components/Body_home/Middle.js
+++ frontend/src/components/Body_home/Middle.js
@@ -10,17 +10,18 @@ let url = String;
 class Middle extends React.Component{
 
   handleClick(){
-    let serveur = document.getElementById('servers').value;
-    let nom = document.getElementById('summoner').value;
-    let section = document.getElementById('fill-in');
+    let servers = document.getElementById('servers').value;
+    let summoner = document.getElementById('summoner').value;
+    let section = document.getElementById('profile');
     const API_DEV = 'RGAPI-0d079bac-c38c-4ae7-a9ec-0e630a4369b5';
-    axios.get(`https://`+serveur+`.api.riotgames.com/lol/summoner/v4/summoners/by-name/`+nom+`?api_key=`+API_DEV)
+    axios.get(`https://`+servers+`.api.riotgames.com/lol/summoner/v4/summoners/by-name/`+summoner+`?api_key=`+API_DEV)
       .then(res => {
         //console.log(res);
         data = res.data;
         url = res.config.url;
         console.log(data, url);
-        section.innerHTML = "Nom: "+String(data.name)+"<br>Niveau: "+String(data.summonerLevel);
+        section.innerHTML = "<div id='Level' class='level_profile'>"+String(data.summonerLevel)+
+        "</div><div id='summoner' class='summoner_profile'>"+String(data.name)+"</div><div id='stats' class='stats_profile'></div>";
       })
       .catch(error => {
         console.log(error.response)
@@ -33,29 +34,29 @@ class Middle extends React.Component{
       <div className='mid-container'>
         <video src={video} autoPlay loop muted/>
         <div className="search">
-        <select id='servers' className="servers">
-            <option value="euw1">EUW</option>
-            <option value="na1">NA</option>
-            <option value="euna1">EUNA</option>
-            <option value="tr1">TR</option>
-            <option value="jp1">JP</option>
-            <option value="oc1">OC</option>
-            <option value="kr">KR</option>
-            <option value="ru">RU</option>
-        </select>
+            <select id='servers' className="servers">
+                <option value="euw1">EUW</option>
+                <option value="na1">NA</option>
+                <option value="euna1">EUNA</option>
+                <option value="tr1">TR</option>
+                <option value="jp1">JP</option>
+                <option value="oc1">OC</option>
+                <option value="kr">KR</option>
+                <option value="ru">RU</option>
+            </select>
 
-        <input
-            type="search"
-            id="summoner"
-            className="input"
-            aria-label="Search through site content"
-            placeholder='Ton pseudo'
-           />
+            <input
+                type="search"
+                id="summoner"
+                className="input"
+                aria-label="Search through site content"
+                placeholder='Ton pseudo'
+               />
 
-        <button onClick={this.handleClick} className="Research">Rechercher</button>
-        <div id='fill-in'>div à remplir avec la requête</div>
+            <button onClick={this.handleClick} className="Research">Rechercher</button>
         </div>
-    </div>
+        <div id='profile'></div>
+      </div>
     );
   }
 }
--- frontend/src/components/pages/Statistiques.js
+++ frontend/src/components/pages/Statistiques.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import '../../App.css';
 import Footer from '../Footer/Footer';
-
 class Statistiques extends React.Component {
   render(){
     return (
       <>
-        <body className='statistiques'>Statistiques</body>
+        <div className="statistiques">Statistiques</div>
         <Footer />
       </>
     )
