4a6808567cfee1aaeb160c5bb95b0176320f9a8a
Christopher Fauconnier <64019350+Lamia-classy@users.noreply.github.com>
2021-04-26 20:27:32 +0200

    Affichage du winrate avec la requête

--- frontend/src/components/Body_home/Middle.js
+++ frontend/src/components/Body_home/Middle.js
@@ -11,28 +11,38 @@ class Middle extends React.Component{
     let server_selected = document.getElementById('servers').value;
     let summoner = document.getElementById('summoner').value;
     let section = document.getElementById('profile');
-    //let summonerId = String;
-    const RiotSummoner = '.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
+    let summonerId = String;
+    const RiotSummoner = '.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
+    const RiotFlex = ".api.riotgames.com/lol/league/v4/entries/by-summoner/";
     //const RiotMastery = '.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/';
-    const RiotFlex = "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/z7X96WuLhmsv_DkGGSzs-AT8syZH8c4W-Vfx9JxbWrBBZ9E?api_key=RGAPI-0d079bac-c38c-4ae7-a9ec-0e630a4369b5"
-    const API_DEV = '?api_key=RGAPI-0d079bac-c38c-4ae7-a9ec-0e630a4369b5';
+    //const RiotFlex = "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/z7X96WuLhmsv_DkGGSzs-AT8syZH8c4W-Vfx9JxbWrBBZ9E?api_key=RGAPI-e3b488b6-d1a1-4862-a595-8b03fcc31f78"
+    const API_DEV = '?api_key=RGAPI-e3b488b6-d1a1-4862-a595-8b03fcc31f78';
 
     axios.get(`https://`+server_selected+RiotSummoner+summoner+API_DEV)
       .then(res => {
         data = res.data;
         //console.log(data);
-        //summonerId = res.data.id;
+        summonerId = res.data.id;
         section.innerHTML = "<div id='Level' class='level_profile'>"+String(data.summonerLevel)+
         "</div><div id='summoner' class='summoner_profile'>"+String(data.name)+"</div><div id='stats' class='stats_profile'></div>";
         //return axios.get(`https://`+server_selected+RiotMastery+summonerId+API_DEV);
-        return axios.get(RiotFlex);
+        return axios.get(`https://`+server_selected+RiotFlex+summonerId+API_DEV);
         })
       .then(res => {
         data = res.data;
+        const stats = document.getElementById('stats');
         for (let i = 0; i < data.length; i++) {
-          console.log(res.data[i])
-          document.getElementById('stats').innerHTML += "Rang: " + data[i].tier + " " + data[i].rank + "<br>";
-          document.getElementById('stats').innerHTML += "Pourcentage de victoire: "+ Math.round(data[i].wins/(data[i].wins+data[i].losses)*100)+"%";
+          //console.log(res.data[i])
+          if(i === 0){
+            stats.innerHTML += "Rang Flex: " + data[i].tier + " " + data[i].rank + "<br>";
+            stats.innerHTML += "Pourcentage de victoire: "+ Math.round(data[i].wins/(data[i].wins+data[i].losses)*100)+"%<br>";
+            stats.innerHTML += "Victoires:"+ String(data[i].wins) + " /Défaites: " + String(data[i].losses)+"<br>";
+          }
+          if (i === 1){
+            stats.innerHTML += "Rang Solo/Duo: " + data[i].tier + " " + data[i].rank + "<br>";
+            stats.innerHTML += "Pourcentage de victoire: "+ Math.round(data[i].wins/(data[i].wins+data[i].losses)*100)+"%<br>";
+            stats.innerHTML += "Victoires:"+ String(data[i].wins) + " /Défaites: " + String(data[i].losses)+"<br>";
+          }
         }
       })
       .catch(error => {
