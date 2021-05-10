04dda6d468e92bd39f605c984d6e91dce075ae01
Christopher Fauconnier <christopherfauconnier@gmail.com>
2021-05-10 15:55:58 +0200

    ajout API historique et détails Django
    2 nouveaux url : history et details et leur fonction dans views.py (dossier api)

--- api/views.py
+++ api/views.py
@@ -7,33 +7,49 @@ from django.http import JsonResponse, HttpResponse
 import requests
 import json
 
+API_DEV = 'api_key=RGAPI-b8f0bd92-b218-4200-8ec9-4528b7b6b082'
+
 
 def profile(request, server, pseudo):
-    API_DEV = '?api_key=RGAPI-cf20f189-4379-4fb0-81a9-f09eb8337db8'
     url = 'https://' + server + \
-        '.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + pseudo + API_DEV
+        '.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + pseudo + '?' + API_DEV
     r = requests.get(url, headers={'Content-Type': 'application/json'})
     account = r.json()
     return JsonResponse(account)
 
 
 def ranks(request, server, summonerId):
-    API_DEV = '?api_key=RGAPI-cf20f189-4379-4fb0-81a9-f09eb8337db8'
     url = 'https://' + server + \
-        '.api.riotgames.com/lol/league/v4/entries/by-summoner/' + summonerId + API_DEV
+        '.api.riotgames.com/lol/league/v4/entries/by-summoner/' + summonerId + '?' + API_DEV
     r = requests.get(url, headers={'Content-Type': 'application/json'})
     ranking = r.json()
     return JsonResponse(ranking, safe=False)
 
 
 def masteries(request, server, summonerId):
-    API_DEV = '?api_key=RGAPI-cf20f189-4379-4fb0-81a9-f09eb8337db8'
     url = 'https://' + server + \
         '.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/' + \
-        summonerId + API_DEV
+        summonerId + '?' + API_DEV
     r = requests.get(url, headers={'Content-Type': 'application/json'})
     data = r.json()
     return JsonResponse(data, safe=False)
+
+
+def history(request, fullserver, summonerPuuid):
+    url = 'https://' + fullserver + \
+        '.api.riotgames.com/lol/match/v5/matches/by-puuid/' + \
+        summonerPuuid + '/ids?start=0&count=10&' + API_DEV
+    r = requests.get(url, headers={'Content-Type': 'application/json'})
+    data = r.json()
+    return JsonResponse(data, safe=False)
+
+
+def historyDetails(request, server, idGame):
+    url = 'https://' + server + \
+        '.api.riotgames.com/lol/match/v4/matches/' + idGame + "?" + API_DEV
+    r = requests.get(url, headers={'Content-Type': 'application/json'})
+    data = r.json()
+    return JsonResponse(data)
 
 
 class UserViewSet(viewsets.ModelViewSet):
